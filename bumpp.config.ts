a4682e035b047a6f8adc2ebda8d1122da9000e18
Barbapapazes <e.soubiran25@gmail.com>
2025-11-21 15:26:13 +0100

    chore: version

--- bumpp.config.ts
+++ bumpp.config.ts
@@ -2,9 +2,5 @@ import { defineConfig } from 'bumpp'
 
 export default defineConfig({
   sign: true,
-  files: [
-    'package.json',
-    'packages/vitepress-plugin-og/package.json',
-    'packages/rspress-plugin-og/package.json',
-  ],
+  recursive: true,
 })
